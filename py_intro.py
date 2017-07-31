d2eec34b1dae16417a320ea0ed5158c01de2a3af
julsmon <juls_m@tiscali.it>
2017-07-31 11:59:23 +0200

    My Django Girls app, first commit

---
+++ py_intro.py
@@ -0,0 +1,11 @@
+#Primo prpgramma in Python. Woo-hoo!
+if 3 < 2:
+    print ("That's right!")
+else:
+    print ("Wrong. Try again!")
+    print ("Come on")
+
+def hi():
+    print ("Hi! "+ " How are you, my darling?")
+
+hi()
